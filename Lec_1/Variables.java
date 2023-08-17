1aa8b446a74d2aaabdcbe639ddd104e09e13e9f0
Ayush-CB <130039413+Ayush-CB@users.noreply.github.com>
2023-08-17 11:33:38 +0530

    Basics

---
+++ Lec_1/Variables.java
@@ -0,0 +1,19 @@
+package Lec_1;
+
+public class Variables {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+        System.out.print("Hello");
+        System.out.println("Hello world");
+        int x=5;
+        x=10;
+        System.out.println(x);
+
+//        int 2c=80;
+//        int cd#=2000;
+        int c2d=230;
+        System.out.println(c2d);
+        int _ad=9000;
+//        int y u=0;
+    }
+}
