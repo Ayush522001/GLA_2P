0df74f7a9cc79e44f7f1ec32bc04ad3226d1e41e
Ayush-CB <130039413+Ayush-CB@users.noreply.github.com>
2023-09-11 14:56:55 +0530

    Functions

---
+++ Lec_11/Functions.java
@@ -0,0 +1,14 @@
+package Lec_11;
+
+public class Functions {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+        sum();
+    }
+    public static void sum(){
+        int a=3;
+        int b=7;
+        int c=a+b;
+        System.out.println(c);
+    }
+}
---
+++ Lec_11/Functions2.java
@@ -0,0 +1,12 @@
+package Lec_11;
+
+public class Functions2 {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+        sum(7,5);
+    }
+    public static void sum(int a,int b){
+        int c=a+b;
+        System.out.println(c);
+    }
+}
