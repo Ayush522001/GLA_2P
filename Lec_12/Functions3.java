9e48fccb36610ae42dd11d8d284dfa9707dc53bc
Ayush-CB <130039413+Ayush-CB@users.noreply.github.com>
2023-09-12 17:01:40 +0530

    Functions

---
+++ Lec_12/Functions3.java
@@ -0,0 +1,13 @@
+package Lec_12;
+
+public class Functions3 {
+    public static void main(String[] args) {
+        int result=add(3,8);
+        System.out.println(result);
+    }
+    public static int add(int a,int b){
+        int c=a+b;
+        return c;
+
+    }
+}
---
+++ Lec_12/Functions4.java
@@ -0,0 +1,18 @@
+package Lec_12;
+
+public class Functions4 {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+        System.out.println(add(4,2));
+        System.out.println("World");
+    }
+    public static int add(int a,int b){
+        int c=a+b;
+        System.out.println(subtract(2,1));;
+        return c;
+    }
+    public static int subtract(int a,int b){
+        int d=a-b;
+        return d;
+    }
+}
