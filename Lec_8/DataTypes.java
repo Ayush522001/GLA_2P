26d87c9ccb332bf4cd38b619156864671ada78d6
Ayush-CB <130039413+Ayush-CB@users.noreply.github.com>
2023-09-12 17:07:31 +0530

    Data Types

---
+++ Lec_8/DataTypes.java
@@ -0,0 +1,15 @@
+package Lec_8;
+
+public class DataTypes {
+    public static void main(String[] args) {
+        byte a=12;
+        short b=20;
+        int c=23;
+        long d=200;
+
+        float dec=2.33f;
+        double db=2.34;
+        char ch='a';
+        boolean cd=true;
+    }
+}
